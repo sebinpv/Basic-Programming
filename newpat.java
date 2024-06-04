76d26bd040f6f566e5cc39cc5844a13c6073beb4
Alby Kennady <alby.kennady@experionglobal.local>
2024-06-04 16:48:30 +0530

    hello

--- newpat.java
+++ newpat.java
@@ -2,9 +2,7 @@ public class NewPattern {
     public static void main(String[] args) {
         int rows = 5; 
 
-        
-
-        
+    
         for (int i = 1; i <= rows; i++) {
            
             for (int j = 1; j <= rows - i; j++) {
